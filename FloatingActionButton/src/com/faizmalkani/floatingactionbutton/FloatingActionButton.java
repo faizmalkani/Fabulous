43f23b8f055f8401a2c7d28d1af2d1cad273f199
Nathan Russak <nathan@bricksimple.com>
2014-08-14 17:13:22 -0400

    Addressed a show/hide animation bug.
    The original code base contained a bug where if the user quickly swiped
    up and down repeatedly on the AbsListView that a FAB was listening to,
    the FAB would lose track of it’s original Y position and would slowly
    scroll off screen never to be seen again. This fix has the FAB make a
    note of it’s displayed Y position after it is assigned in onLayout()
    and use it when animating instead of it’s current Y position.

--- FloatingActionButton/src/com/faizmalkani/floatingactionbutton/FloatingActionButton.java
+++ FloatingActionButton/src/com/faizmalkani/floatingactionbutton/FloatingActionButton.java
@@ -25,22 +25,24 @@ public class FloatingActionButton extends View {
     private final Paint mButtonPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final Paint mDrawablePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Bitmap mBitmap;
-    private int mScreenHeight;
     private int mColor;
-    private float mCurrentY;
     private boolean mHidden = false;
-
+    /** The FAB button's Y position when it is displayed. */
+    private float mYDisplayed = -1;
+    /** The FAB button's Y position when it is hidden. */
+    private float mYHidden = -1;
+    
     public FloatingActionButton(Context context) {
         this(context, null);
     }
-
+    
     public FloatingActionButton(Context context, AttributeSet attributeSet) {
         this(context, attributeSet, 0);
     }
-
+    
     public FloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
+        
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FloatingActionButton);
         mColor = a.getColor(R.styleable.FloatingActionButton_color, Color.WHITE);
         mButtonPaint.setStyle(Paint.Style.FILL);
@@ -51,43 +53,56 @@ public class FloatingActionButton extends View {
         dy = a.getFloat(R.styleable.FloatingActionButton_shadowDy, 3.5f);
         int color = a.getInteger(R.styleable.FloatingActionButton_shadowColor, Color.argb(100, 0, 0, 0));
         mButtonPaint.setShadowLayer(radius, dx, dy, color);
-
+        
         Drawable drawable = a.getDrawable(R.styleable.FloatingActionButton_drawable);
         if (null != drawable) {
             mBitmap = ((BitmapDrawable) drawable).getBitmap();
         }
         setWillNotDraw(false);
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-
+        
         WindowManager mWindowManager = (WindowManager)
-                context.getSystemService(Context.WINDOW_SERVICE);
+        context.getSystemService(Context.WINDOW_SERVICE);
         Display display = mWindowManager.getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        mScreenHeight = size.y;
+        mYHidden = size.y;
     }
-
+    
     public void setColor(int color) {
         mColor = color;
         mButtonPaint.setColor(mColor);
         invalidate();
     }
-
+    
     public void setDrawable(Drawable drawable) {
         mBitmap = ((BitmapDrawable) drawable).getBitmap();
         invalidate();
     }
-
-
+    
+    
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawCircle(getWidth() / 2, getHeight() / 2, (float) (getWidth() / 2.6), mButtonPaint);
         if (null != mBitmap) {
             canvas.drawBitmap(mBitmap, (getWidth() - mBitmap.getWidth()) / 2,
-                    (getHeight() - mBitmap.getHeight()) / 2, mDrawablePaint);
+                              (getHeight() - mBitmap.getHeight()) / 2, mDrawablePaint);
         }
     }
-
+    
+    @Override protected void onLayout (boolean changed, int left, int top, int right, int bottom)
+    {
+        // Perform the default behavior
+        super.onLayout(changed, left, top, right, bottom);
+        
+        // Store the FAB button's displayed Y position if we are not already aware of it
+        if (mYDisplayed == -1)
+        {
+            mYDisplayed = this.getY();
+        }
+    }
+    
+    
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int color;
@@ -100,29 +115,27 @@ public class FloatingActionButton extends View {
         invalidate();
         return super.onTouchEvent(event);
     }
-
+    
     public void hide(boolean hide) {
+        // If the hidden state is being updated
         if (mHidden != hide) {
-            float offset;
-            if (mHidden) {
-                offset = mCurrentY;
-            } else {
-                mCurrentY = getY();
-                offset = mScreenHeight;
-            }
+            
+            // Store the new hidden state
             mHidden = hide;
-            ObjectAnimator animator = ObjectAnimator.ofFloat(this, "Y", offset);
+            
+            // Animate the FAB to it's new Y position
+            ObjectAnimator animator = ObjectAnimator.ofFloat(this, "Y", mHidden ? mYHidden : mYDisplayed);
             animator.setInterpolator(mInterpolator);
             animator.start();
         }
     }
-
+    
     public void listenTo(AbsListView listView) {
         if (null != listView) {
             listView.setOnScrollListener(new DirectionScrollListener(this));
         }
     }
-
+    
     public static int darkenColor(int color) {
         float[] hsv = new float[3];
         Color.colorToHSV(color, hsv);
